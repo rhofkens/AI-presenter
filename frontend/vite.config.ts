9363d93bcdaa11b621ec36d9c08e218196814212
Roeland Hofkens <roeland.hofkens@bluefields.ai>
2025-04-04 15:03:27 +0200

    Update vite build output to target/classes/static

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../src/main/resources/static'
+    outDir: resolve(__dirname, '../target/classes/static')
   },
   resolve: {
     alias: {
